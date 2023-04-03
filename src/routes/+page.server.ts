7091c9f3393aecb1965af9f3f4c23ee016289c29
KyleKiTheodo <kylek@theodo.com>
2023-04-03 17:39:33 -0400

    Added logic from previous repo

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import { HUBSPOT_FORM_ID, HUBSPOT_PRIVATE_APP_KEY } from '$env/static/private';
+
+interface FormFieldGroups {
+	fields: {
+		name: string;
+		label: string;
+		type: string;
+		fieldType: string;
+		description: string;
+		groupName: string;
+		displayOrder: number;
+		required: boolean;
+		// selectedOptions: [];
+		options: { value: string; label: string }[]; // Not sure
+		// validation: [Object];
+		enabled: boolean;
+		hidden: boolean;
+		defaultValue: string;
+		isSmartField: boolean;
+		unselectedLabel: string;
+		placeholder: string;
+		// dependentFieldFilters: [];
+		labelHidden: boolean;
+		propertyObjectType: string;
+		// metaData: [];
+		objectTypeId: string;
+	}[];
+}
+
+export async function load() {
+	const response = await axios.get<{ formFieldGroups: FormFieldGroups[] }>(
+		`https://api.hubapi.com/forms/v2/forms/${HUBSPOT_FORM_ID}`,
+		{
+			headers: {
+				Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_KEY}`
+			}
+		}
+	);
+	return { formFieldGroups: response.data.formFieldGroups };
+}
+
+export const actions = {
+	submit: async ({ request }) => {
+		const data = await request.formData();
+		console.log(data);
+		// console.log(...data);
+
+		// const fields = [];
+
+		// for (const el of data) {
+		// 	fields.push({
+		// 		name: el[0],
+		// 		value: el[1],
+		// 		objectTypeId: '0-1'
+		// 	});
+		// }
+
+		// console.log(`The body is ${JSON.stringify({ body: fields })}`);
+
+		// UNTESTED
+		// const response = await axios.post(
+		// 	`https://api.hsforms.com/submissions/v3/integration/submit/${HUBSPOT_PORTAL_ID}/${HUBSPOT_FORM_ID}`,
+		// 	{ body: fields },
+		// 	{
+		// 		headers: {
+		// 			Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_KEY}`
+		// 		}
+		// 	}
+		// );
+
+		// console.log(response);
+
+		return { success: true };
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>Get your refund</h1>
+<form action="?/submit" method="POST">
+	{#each data.formFieldGroups as group}
+		<fieldset>
+			{#each group.fields as field}
+				<label for={field.name}>{field.label}</label>
+				{#if field.fieldType === 'select'}
+					<select name={field.name} id={field.name}>
+						{#each field.options as option}
+							<option value={option.value}>{option.label}</option>
+						{/each}
+					</select>
+				{:else}
+					<input type={field.fieldType} name={field.name} id={field.name} />
+				{/if}
+			{/each}
+		</fieldset>
+	{/each}
+	<button type="submit">Submit</button>
+</form>
