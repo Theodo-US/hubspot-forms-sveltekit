cb6d5c0a58c75bb779f56aa95d66d0961931f123
KyleKiTheodo <kylek@theodo.com>
2023-04-05 12:08:17 -0400

    Initial setup for artillery

--- tests/cloudflare.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://hubspot-forms-sveltekit.pages.dev/');
-});
--- tests/hubspot.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://share.hsforms.com/1JFQbjrblSeCcRLwUvqqLEAebia9');
-});
